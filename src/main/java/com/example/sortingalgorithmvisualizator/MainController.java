8743f9b769b910c4b4375cb4e156602aabf7eb21
PhilKnights <filippocavalieri3510@gmail.com>
2023-06-23 14:29:39 +0200

    Files renamed

--- src/main/java/com/example/sortingalgorithmvisualizator/MainController.java
+++ src/main/java/com/example/sortingalgorithmvisualizator/MainController.java
@@ -1,8 +1,13 @@
 package com.example.sortingalgorithmvisualizator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class MainController {
+
+    @FXML
+    void onClick(ActionEvent event) {
+
+    }
 
 }
