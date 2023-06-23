205d9cb2aad82fbee99ef0ad8d863f3f074e61df
PhilKnights <filippocavalieri3510@gmail.com>
2023-06-23 14:24:05 +0200

    Files renamed

--- src/main/java/com/example/sortingalgorithmvisualizator/MainController.java
+++ src/main/java/com/example/sortingalgorithmvisualizator/MainController.java
@@ -4,11 +4,5 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
