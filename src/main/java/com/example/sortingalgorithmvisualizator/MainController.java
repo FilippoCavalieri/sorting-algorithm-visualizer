670ac14283af2ab6425c8f8b8665e9273321ae64
PhilKnights <filippocavalieri3510@gmail.com>
2023-07-03 12:12:37 +0200

    added listeners

--- src/main/java/com/example/sortingalgorithmvisualizator/MainController.java
+++ src/main/java/com/example/sortingalgorithmvisualizator/MainController.java
@@ -32,6 +32,7 @@ public class MainController {
     private final int DEFAULT_ARRAY_RANGE = 100;
     private int barsNumber, valueRange;
     private String sortingAlgorithm;
+
     @FXML
     public void initialize() {
         xAxis = new CategoryAxis();
@@ -39,32 +40,43 @@ public class MainController {
         BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis);
 
         arraySizeSlider.setValue(DEFAULT_ARRAY_SIZE);
+        arraySizeValueLabel.setText(Integer.toString(DEFAULT_ARRAY_SIZE));
         arrayRangeSlider.setValue(DEFAULT_ARRAY_RANGE);
+        arrayRangeValueLabel.setText(Integer.toString(DEFAULT_ARRAY_RANGE));
 
         barsNumber = DEFAULT_ARRAY_SIZE;
         valueRange = DEFAULT_ARRAY_RANGE;
 
         initializeBarChart();
         fillArray();
+
+        arraySizeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            barsNumber = (int) arraySizeSlider.getValue();
+            arraySizeValueLabel.setText(Integer.toString(barsNumber));
+        });
+
+        arrayRangeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            valueRange = (int) arrayRangeSlider.getValue();
+            arrayRangeValueLabel.setText(Integer.toString(valueRange));
+        });
     }
 
-    public void initializeBarChart(){
+    public void initializeBarChart() {
         barChart.setBarGap(0);
     }
 
-    public void fillArray(){
+    public void fillArray() {
         XYChart.Series<String, Integer> series = new XYChart.Series<>();
-
-        for(int i = 0; i < barsNumber; i++) {
+        for (int i = 0; i < barsNumber; i++) {
             series.getData().add(new XYChart.Data<>(Integer.toString(i + 1), new Random().nextInt(valueRange)));
         }
 
         barChart.getData().add(series);
     }
+
     @FXML
-    public void handleReset(){
+    public void handleReset() {
         barChart.getData().clear();
         fillArray();
     }
-
 }
